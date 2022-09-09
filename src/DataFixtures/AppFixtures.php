539906b8d9a1843a7ff89aafc63d37253cc50a9b
Alban <axhepi21@gmail.com>
2022-09-09 11:26:28 +0200

    created user factory and populated the db with fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,6 +9,7 @@ use App\Entity\Reservation;
 use App\Entity\Room;
 use App\Factory\BuildingFactory;
 use App\Factory\RoomFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use function Zenstruck\Foundry\faker;
@@ -20,58 +21,62 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
        // BuildingFactory::new()->createMany(4);
+//
+//        $admin= new Admin();
+//        $admin->setName('Mario');
+//        $admin->setSurname('Bregu');
+//        $admin->setEmail('mario@gmail.com');
+//        $admin->setPassword('password');
+//        $admin->setBirthday(faker()->dateTimeInInterval('-20 years'));
+//
+//
+//        $building = new Building();
+//        $building->setName('Freedom');
+//        $building->setAddress('Don Bosko');
+//        $building->setOwner($admin);
+//
+//        $building1 = new Building();
+//        $building1->setName('Freedom');
+//        $building1->setAddress('Don Bosko');
+//        $building1->setOwner($admin);
+//
+//
+//
+//        $room = new Room();
+//
+//        $status = [1,1,1,1,1,1,1];
+//        $room->setName('A23');
+//        $room->setBuilding($building);
+//        $room->setCapacity(10);
+//        $room->setStatus($status);
+//
+//        $personnel = new Personnel();
+//        $personnel->setName('Alban');
+//        $personnel->setSurname('Xhepi');
+//        $personnel->setEmail('albanxhepi@gmail.com');
+//        $personnel->setBirthday(faker()->dateTimeInInterval("-20 years"));
+//        $personnel->setPassword('helloWorld');
+//
+//        $reservation = new Reservation();
+//
+//        $reservation->setDate(faker()->dateTimeInInterval('-1 year'));
+//         $reservation->setRoom($room);
+//         $reservation->setState('Free');
+//         $reservation->addPersonnel($personnel);
+//
+//
+//
+//
+//
+//         $manager->persist($admin);
+//         $manager->persist($personnel);
+//        $manager->persist($building);
+//        $manager->persist($room);
+//        $manager->persist($reservation);
 
-        $admin= new Admin();
-        $admin->setName('Mario');
-        $admin->setSurname('Bregu');
-        $admin->setEmail('mario@gmail.com');
-        $admin->setPassword('password');
-        $admin->setBirthday(faker()->dateTimeInInterval('-20 years'));
-
-
-        $building = new Building();
-        $building->setName('Freedom');
-        $building->setAddress('Don Bosko');
-        $building->setOwner($admin);
-
-        $building1 = new Building();
-        $building1->setName('Freedom');
-        $building1->setAddress('Don Bosko');
-        $building1->setOwner($admin);
-
-
-
-        $room = new Room();
-
-        $status = [1,1,1,1,1,1,1];
-        $room->setName('A23');
-        $room->setBuilding($building);
-        $room->setCapacity(10);
-        $room->setStatus($status);
-
-        $personnel = new Personnel();
-        $personnel->setName('Alban');
-        $personnel->setSurname('Xhepi');
-        $personnel->setEmail('albanxhepi@gmail.com');
-        $personnel->setBirthday(faker()->dateTimeInInterval("-20 years"));
-        $personnel->setPassword('helloWorld');
-
-        $reservation = new Reservation();
-
-        $reservation->setDate(faker()->dateTimeInInterval('-1 year'));
-         $reservation->setRoom($room);
-         $reservation->setState('Free');
-         $reservation->addPersonnel($personnel);
-
-
-
-
-
-         $manager->persist($admin);
-         $manager->persist($personnel);
-        $manager->persist($building);
-        $manager->persist($room);
-        $manager->persist($reservation);
+        
+        UserFactory::createOne(['email' => 'albanxhepi@gmail.com']);
+        UserFactory::createMany(10);
 
          $manager->flush();
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface
+class   User implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
